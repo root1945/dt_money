ed9b42af74c6092453566f03e6d5ee39687c4999
Victor Alencar <munternl13@gmail.com>
2021-10-10 22:46:24 -0300

    adicionado os summarys no dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import {GlobalStyled} from './styles/global'
 export function App() {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyled/>
     </>
   );
---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,31 @@
+import {Container} from './styles'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import totalImg from '../../assets/total.svg'
+export default function Summary(){
+  return (
+    <Container>
+      <div>
+        <header>
+          <img src={incomeImg} alt="Entradas" />
+          <p>Entradas</p>
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+      <div>
+        <header>
+          <img src={outcomeImg} alt="Saídas" />
+          <p>Saídas</p>
+        </header>
+        <strong> -R$400,00</strong>
+      </div>
+      <div className="highlight-background">
+        <header>
+          <img src={totalImg} alt="Total" />
+          <p>Total</p>
+        </header>
+        <strong>R$2000,00</strong>
+      </div>
+    </Container>
+  );
+}
